fef8891ad907f74eea865ef7e002208f01e19252
Jang-Tae-Young <tyjang1020@gmail.com>
2020-08-02 16:50:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_results = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-print(indeed_results.headers)
+indeed_soup = BeautifulSoup(indeed_results.text,'html.parser')
+
+
+print(indeed_soup.title.string)
+
+
 
